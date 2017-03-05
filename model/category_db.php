db08f522e28c706f6d733d90e077e8a642df9e4c
ruzbeh sharukh kermanian <rk592@afsconnect2.njit.edu>
2017-03-05 18:54:21 -0500

    Step5: Adding tow functions

--- model/category_db.php
+++ model/category_db.php
@@ -20,4 +20,25 @@ function get_category_name($category_id) {
     $category_name = $category['categoryName'];
     return $category_name;
 }
+
+function add_category($category_name) {
+   global $db;
+   $query = " INSERT INTO categories_guitar1 (categoryName) values (:category_name)";
+   $statement = $db->prepare($query);
+   $statement->bindValue(':category_name', $category_name);
+   $statement->execute();
+   $statement->closeCursor();
+
+}
+
+function delete_category($category_id) {
+
+   global $db;
+   $query = "DELETE FROM categories_guitar1 WHERE categoryID = :category_id";
+   $statement = $db->prepare($query);
+   $statement->bindValue(':category_id', $category_id);
+   $statement->execute();
+   $statement->closeCursor();
+
+}
 ?>
